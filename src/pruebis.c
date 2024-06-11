10e7eb6e22738ef347c55bf3f403f6c0ed816ef4
Delfina Maria Luz Parada <dparada@c1r3s1.42malaga.com>
2024-06-11 18:55:41 +0200

    comillas listas by sam

--- src/pruebis.c
+++ src/pruebis.c
@@ -6,7 +6,7 @@
 /*   By: dparada <dparada@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 14:58:49 by dparada           #+#    #+#             */
-/*   Updated: 2024/06/11 17:40:26 by dparada          ###   ########.fr       */
+/*   Updated: 2024/06/11 18:55:37 by dparada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,6 +58,7 @@ char	*copy_quote(char *line, char d, char s, int j)
 		else if (line[i] == d || line[i] == s)
 		{
 			c = line[i];
+			
 			while (line[i] == c)
 				i++;
 		}
